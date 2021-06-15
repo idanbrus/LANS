34ffb224ad5ee737c193faeebf1b705d8136da02
idanbrus <brusli1@gmail.com>
2021-06-15 14:34:03 +0300

    fixed the encoder

--- lans/models/encoder.py
+++ lans/models/encoder.py
@@ -10,24 +10,14 @@ class Encoder(nn.Module):
         self.char_embedder = torch.nn.Embedding(n_charecters, input_size)
         self.relu1 = torch.nn.ReLU()
         self.token_linear = torch.nn.Linear(embedding_dim, hidden_size)
-        self.bilstm = torch.nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
-        self.hidden_combine = torch.nn.Linear(hidden_size * 2, hidden_size)
-        self.output_combine = torch.nn.Linear(hidden_size * 2, hidden_size)
+        self.char_linear = torch.nn.Linear(input_size, input_size)
+        self.encoder_lstm = torch.nn.LSTM(input_size, hidden_size, batch_first=True)
 
     def forward(self, input_chars, token_embedding):
         input = self.char_embedder(input_chars)
+        input = self.char_linear(input)
         h_0 = self.token_linear(token_embedding).unsqueeze(0)
         h_0 = self.relu1(h_0)
         c_0 = torch.zeros_like(h_0)
-
-        # insert hidden layers from both sides of BiLSTM
-        h_0, c_0 = torch.cat([h_0, h_0]), torch.cat([c_0, c_0])
-
-        output, (hidden, cell_state) = self.bilstm(input, (h_0, c_0))
-
-        # change back to single dimension (like one-way LSTM)
-        output = self.output_combine(output)
-        hidden = torch.cat((hidden[0], hidden[1]), dim=-1).unsqueeze(0)
-        hidden = self.hidden_combine(hidden)
-
+        output, (hidden, cell_state) = self.encoder_lstm(input, (h_0, c_0))
         return output, (hidden, cell_state)
